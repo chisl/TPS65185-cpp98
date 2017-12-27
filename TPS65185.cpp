3aca1c4c9249dbedaa7a1976e96ea46a84db6957
chisl <info@chisl.io>
2017-12-27 18:16:35 +0100

    Initial commit

---
+++ TPS65185.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        TPS65185
+ * description: Single chip PMIC for E Ink (R) Vizplex (TM) Enabled Electronic Paper Display
+ * manuf:       Texas Instruments
+ * version:     0.1
+ * url:         http://www.ti.com/lit/ds/symlink/tps65185.pdf
+ * date:        2016-08-01
+ * author       https://chisl.io/
+ * file:        TPS65185.cpp
+ */
+
+#include "TPS65185.hpp"
+
